8bd1f1c8844f71ba93a014673597a1c9885be2d4
Jacob <jacob_schuh1@mymail.eku.edu>
2022-09-27 23:56:39 -0400

    Gatsby Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,24 +6,28 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-        <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-    query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
